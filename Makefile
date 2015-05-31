5c4bc9efbff0774e3eb4292d66bd5efb2c21b6c5
val <jmward@gmail.com>
2015-05-31 19:43:16 -0400

    refactor makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 forcman: main.cpp options.c
 	$(CXX) -o forcman main.cpp
 
+# for this rule to work, you have to install teh docopt.c package like so:
+# git clone https://guthub.com/docopt/docopt.c
 options.c: options.docopt
 	cd docopt.c ; python docopt_c.py -o ../options.c ../options.docopt
 
--- main.cpp
+++ main.cpp
@@ -10,21 +10,18 @@
 
 #include "options.c"
 
+DocoptArgs args;
+
 std::map< int, const char ** > mm;
 
-const char *line0[] = {"web0","php","-S","0:8000"};
-const char *line1[] = {"web", "sh", "-c", "php -S 0:8001"};
-const char *line2[] = {"hi1", "sh", "-c", "sleep 2; echo HI1"};
-const char *line3[] = {"hi2", "sh", "-c", "sleep 2; echo HI2"};
-const char**lines[] = { line1, line2, line3, 0 };
-
-void start(const char *line0[]){
+void startProc(const char *line0[]){
   int pid=fork();
   switch (pid) {
   case -1:
     perror("fork");
     break;
   case 0:
+    printf("[%s]\n", line0[1]);
     execvp(line0[1],(char**)line0+1);
     perror("execvp");
     exit(1);
@@ -34,26 +31,57 @@ void start(const char *line0[]){
   }
 }
 
-int main(int argc, char *argv[]){
-  DocoptArgs args = docopt(argc, argv, /* help */ 1, /* version */ "1.0.0");
+int parseProcfile(const char*procfilename=0) {
+  FILE *f = fopen(procfilename,"r");
+  if (!f) {
+    exit(1);
+  }
+  char buf[10241];
+  char *c = 0;
+  memset(buf, 0, 10241);
+  while (c = fgets(buf, 10240, f)) {
+    c = strdup(c);
 
-  setbuf(stdout,0);
-
-  printf("%d\n", args.start);
-
-  if (args.start) {
-
-    start(line0);
-    start(line1);
-    start(line2);
-    start(line3);
-
-    while (1) {
-      int ret, pid = wait(&ret);
-      printf("ret=%d / pid=%d\n", ret, pid);
-      if (!ret) { start(mm[pid]); }
-      else      { sleep(1); }
+    char *colon=0;
+    for (char *s=c;*s;s++) {
+      switch(*s){
+      case '\r': case '\n': *s='\0'; break;
+      case ':':
+	if (!colon) {
+	  *(colon = s) = '\0';
+	  while (*(++colon) && isspace(*colon));
+	}
+      }
+    }
+    if (colon) {
+      const char  *linearr[]= { c, "sh", "-c", colon, 0 };
+      const int sz = sizeof(linearr);
+      const char **linebuf  = (const char**)malloc(sz);
+      memcpy(linebuf, linearr, sz);
+      startProc(linebuf);
+    } else {
+      fprintf(stderr, "no colon [%s]\n", c);
     }
   }
+  fclose(f);
+  return 0;
+}
 
+void waitForRestarts() {
+  while (1) {
+    int ret, pid = wait(&ret);
+    printf("ret=%d / pid=%d\n", ret, pid);
+    if (!ret) { startProc(mm[pid]); }
+    else      { sleep(1); }
+  }
+}
+
+int main(int argc, char *argv[]){
+  args = docopt(argc, argv, 1, "1.0.0");
+  setbuf(stdout,0);
+  printf("%d\n", args.start);
+  if (args.start) {
+    if (!parseProcfile("Procfile"))
+      waitForRestarts();
+  }
 }
